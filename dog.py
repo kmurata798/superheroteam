2f17a5d0837f376ea7db058fc8735554d037e347
kmurata798 <kmurata798@gmail.com>
2019-09-22 17:51:38 -0700

    first commit

---
+++ dog.py
@@ -0,0 +1,8 @@
+# dog.py
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("woof!")
---
+++ my-dog.py
@@ -0,0 +1,9 @@
+import dog
+
+my_dog = Dog("Rex", "SuperDog")
+#my_dog.breed = "SuperDog"
+print(my_dog)
+print(my_dog.name)
+print(my_dog.color)
+print(my_dog.breed)
+my_dog.bark()
